c7bfa4ad20a85cdb47a67b0c4e7e98e45279c18f
Jordan Tseng <tingchun0131@gmail.com>
2020-07-19 11:04:53 +0800

    修改按鈕的event.preventDefault() ，同時新增21關的新增BMI 平均數

--- BMI/app/assets/js/all.js
+++ BMI/app/assets/js/all.js
@@ -1,12 +1,32 @@
 $(document).ready(() => {
+    //請解讀 Vic 的(程式碼)，並在 Slack 上回報他以下邏輯是做了什麼事情：
+
+    // arrayBMIrecord 的陣列用途是什麼？
+    //用來儲存每次重新整理之前的BMI查詢記錄
+    // 請描述 calculateBMI 函式做了什麼事情
+    // 1. 先取值
+    // 2. 把數值計算成BMI值
+    // 3. 宣告一個空變數，內容包含[身高、體重、BMI、title]
+    // 4. 把身高、體重、BMI值存在那個空變數，
+    // 5. if判斷BMI屬於哪種title，知道之後賦值給status.title
+    // 6. 把結果push去arrayBMIrecord
+    // 7. 執行render()函式
+    // 8. 把input清空
+    // 請描述 render 函式做了什麼事情
+    //render主要用來產生innerHtml，顯示結果
+
     //1.先取得DOM上面的元件
     var btnBmiSubmit = document.querySelector('#btnBmiSubmit')
     var btnBmiDelete = document.querySelector('#btnBmiDelete')
     var bmiHeightNum = document.querySelector('#bmiHeightNum')
     var bmiWeightNum = document.querySelector('#bmiWeightNum')
     var recordList = document.querySelector('.recordList')
+    var bmiAvarge = document.querySelector('.bmiAvarge')
+
     //2.建立array
     var arrayBmiRecord = []
+    var ArrayBmiNum = []
+
     //3.建立 BmiRecordData的各種類型
     var BmiRecordData = {
         tooThin: {
@@ -34,21 +54,45 @@ $(document).ready(() => {
             text +=
                 '<li class="recordBox"><ul><li class="recordBox__title">' +
                 item.title +
-                '</li><li class="recordBox__text">BMI:' +
+                '</li><li class="recordBox__text">BMI：' +
                 item.BMI +
                 '</li><li class="recordBox__text">height：' +
-                item.BMI +
+                item.height +
                 '</li><li class="recordBox__text">weight：' +
-                item.BMI +
+                item.weight +
                 '</li></ul></li>'
         })
-        alert(text)
+        // console.log(text)
+        // console.log(arrayBmiRecord)
         recordList.innerHTML = text
     }
-    // function textDelete(){
+    function textDelete() {
+        //超級重要的停止事件
+        event.preventDefault()
+        recordList.innerHTML = ''
+    }
+    function bmiAvargeCount() {
+        var bmiCountTime = arrayBmiRecord.length
+        var Num = 0
+        var bmiAvargeNum
+        // console.log(arrayBmiRecord)
+        arrayBmiRecord.forEach(function (item) {
+            // ArrayBmiNum.push(item.BMI)
+            // console.log(ArrayBmiNum)
+            Num += item.BMI
+            // alert(typeof(item.BMI))
+            // alert(Num)
+        })
+        // alert(Num)
+        bmiAvargeNum = Num / bmiCountTime
+        // alert(bmiAvargeNum)
+        bmiAvarge.textContent = '總共統計' + bmiCountTime + '次，平均BMI為' + bmiAvargeNum + '。'
+    }
 
-    // }
     function bmiCount() {
+        //超級重要的停止事件
+        event.preventDefault()
+
         var userRecord = {
             title: '',
             height: '',
@@ -59,9 +103,8 @@ $(document).ready(() => {
         var weightNum = parseInt(bmiWeightNum.value)
         userRecord.height = heightNum
         userRecord.weight = weightNum
-        userRecord.BMI = bmi
         var bmi = weightNum / (((heightNum / 100) * heightNum) / 100)
-
+        userRecord.BMI = Math.round(bmi)
         if (bmi >= 35) {
             userRecord.title = BmiRecordData.overFat.title
         } else if (30 <= bmi && bmi < 35) {
@@ -77,11 +120,13 @@ $(document).ready(() => {
         }
         //推送進去陣列
         arrayBmiRecord.push(userRecord)
-        console.log(arrayBmiRecord)
         // 渲染到html
         render()
+        bmiAvargeCount()
+        bmiWeightNum.value = ''
+        bmiHeightNum.value = ''
     }
     //監聽事件表
     btnBmiSubmit.addEventListener('click', bmiCount)
-    // btnBmiDelete.addEventListener('click',textDelete);
+    btnBmiDelete.addEventListener('click', textDelete)
 })
--- BMI/dist/assets/js/all.js
+++ BMI/dist/assets/js/all.js
@@ -1,14 +1,30 @@
 "use strict";
 
 $(document).ready(function () {
+  //請解讀 Vic 的(程式碼)，並在 Slack 上回報他以下邏輯是做了什麼事情：
+  // arrayBMIrecord 的陣列用途是什麼？
+  //用來儲存每次重新整理之前的BMI查詢記錄
+  // 請描述 calculateBMI 函式做了什麼事情
+  // 1. 先取值
+  // 2. 把數值計算成BMI值
+  // 3. 宣告一個空變數，內容包含[身高、體重、BMI、title]
+  // 4. 把身高、體重、BMI值存在那個空變數，
+  // 5. if判斷BMI屬於哪種title，知道之後賦值給status.title
+  // 6. 把結果push去arrayBMIrecord
+  // 7. 執行render()函式
+  // 8. 把input清空
+  // 請描述 render 函式做了什麼事情
+  //render主要用來產生innerHtml，顯示結果
   //1.先取得DOM上面的元件
   var btnBmiSubmit = document.querySelector('#btnBmiSubmit');
   var btnBmiDelete = document.querySelector('#btnBmiDelete');
   var bmiHeightNum = document.querySelector('#bmiHeightNum');
   var bmiWeightNum = document.querySelector('#bmiWeightNum');
-  var recordList = document.querySelector('.recordList'); //2.建立array
+  var recordList = document.querySelector('.recordList');
+  var bmiAvarge = document.querySelector('.bmiAvarge'); //2.建立array
 
-  var arrayBmiRecord = []; //3.建立 BmiRecordData的各種類型
+  var arrayBmiRecord = [];
+  var ArrayBmiNum = []; //3.建立 BmiRecordData的各種類型
 
   var BmiRecordData = {
     tooThin: {
@@ -34,15 +50,39 @@ $(document).ready(function () {
   function render() {
     var text = '';
     arrayBmiRecord.forEach(function (item) {
-      text += '<li class="recordBox"><ul><li class="recordBox__title">' + item.title + '</li><li class="recordBox__text">BMI:' + item.BMI + '</li><li class="recordBox__text">height：' + item.BMI + '</li><li class="recordBox__text">weight：' + item.BMI + '</li></ul></li>';
-    });
-    alert(text);
-    recordList.innerHTML = text;
-  } // function textDelete(){
-  // }
+      text += '<li class="recordBox"><ul><li class="recordBox__title">' + item.title + '</li><li class="recordBox__text">BMI：' + item.BMI + '</li><li class="recordBox__text">height：' + item.height + '</li><li class="recordBox__text">weight：' + item.weight + '</li></ul></li>';
+    }); // console.log(text)
+    // console.log(arrayBmiRecord)
 
+    recordList.innerHTML = text;
+  }
+
+  function textDelete() {
+    //超級重要的停止事件
+    event.preventDefault();
+    recordList.innerHTML = '';
+  }
+
+  function bmiAvargeCount() {
+    var bmiCountTime = arrayBmiRecord.length;
+    var Num = 0;
+    var bmiAvargeNum; // console.log(arrayBmiRecord)
+
+    arrayBmiRecord.forEach(function (item) {
+      // ArrayBmiNum.push(item.BMI)
+      // console.log(ArrayBmiNum)
+      Num += item.BMI; // alert(typeof(item.BMI))
+      // alert(Num)
+    }); // alert(Num)
+
+    bmiAvargeNum = Num / bmiCountTime; // alert(bmiAvargeNum)
+
+    bmiAvarge.textContent = '總共統計' + bmiCountTime + '次，平均BMI為' + bmiAvargeNum + '。';
+  }
 
   function bmiCount() {
+    //超級重要的停止事件
+    event.preventDefault();
     var userRecord = {
       title: '',
       height: '',
@@ -53,8 +93,8 @@ $(document).ready(function () {
     var weightNum = parseInt(bmiWeightNum.value);
     userRecord.height = heightNum;
     userRecord.weight = weightNum;
-    userRecord.BMI = bmi;
     var bmi = weightNum / (heightNum / 100 * heightNum / 100);
+    userRecord.BMI = Math.round(bmi);
 
     if (bmi >= 35) {
       userRecord.title = BmiRecordData.overFat.title;
@@ -71,14 +111,17 @@ $(document).ready(function () {
     } //推送進去陣列
 
 
-    arrayBmiRecord.push(userRecord);
-    console.log(arrayBmiRecord); // 渲染到html
+    arrayBmiRecord.push(userRecord); // 渲染到html
 
     render();
+    bmiAvargeCount();
+    bmiWeightNum.value = '';
+    bmiHeightNum.value = '';
   } //監聽事件表
 
 
-  btnBmiSubmit.addEventListener('click', bmiCount); // btnBmiDelete.addEventListener('click',textDelete);
+  btnBmiSubmit.addEventListener('click', bmiCount);
+  btnBmiDelete.addEventListener('click', textDelete);
 });
 "use strict";
 
